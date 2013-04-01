f2ddeb40705b936c6bd5e8873f70fe73ae8cdd04
Tobias Baum <tbaum@tntinteractive.de>
2013-04-01 19:51:51 +0200

    Implementation for [JENKINS-15248], untested

--- src/main/java/hudson/plugins/violations/ViolationsPublisher.java
+++ src/main/java/hudson/plugins/violations/ViolationsPublisher.java
@@ -11,10 +11,12 @@ import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.Action;
 import hudson.model.BuildListener;
+import hudson.model.Result;
 import hudson.model.AbstractProject;
 import hudson.tasks.Publisher;
 import hudson.tasks.Recorder;
 
+import hudson.plugins.violations.ViolationsReport.TypeReport;
 import hudson.plugins.violations.hudson.ViolationsFreestyleDescriptor;
 import hudson.tasks.BuildStepDescriptor;
 
@@ -78,10 +80,49 @@ public class ViolationsPublisher extends Recorder {
         report.setBuildResult();
         build.getActions().add(
             new ViolationsBuildAction(build, report));
+        handleRatcheting(report, listener);
         return true;
     }
 
     /**
+     * Perform ratcheting if enabled, i.e. lower the thresholds if the build is
+     * stable and the current value is lower than the current threshold.
+     */
+    private void handleRatcheting(ViolationsReport report, BuildListener listener) {
+        //don't do anything if ratcheting is completely disabled
+        if (!this.getConfig().isAutoUpdateMax() && !this.getConfig().isAutoUpdateUnstable()) {
+            return;
+        }
+
+        //don't change the values if the build is not stable
+        if (report.getBuild().getResult() != Result.SUCCESS) {
+            return;
+        }
+
+        //adjust the single configs (if needed)
+        for (TypeReport typeReport : report.getTypeReports().values()) {
+            TypeConfig typeConfig = this.getConfig().getTypeConfigs().get(typeReport.getType());
+            int thresholdCount = typeReport.getNumber() + 1;
+
+            if (this.getConfig().isAutoUpdateUnstable() && thresholdCount < typeConfig.getUnstable()) {
+                listener.getLogger().println("Setting unstable value for " 
+                        + typeConfig.getType() + " to " + thresholdCount);
+                typeConfig.setUnstable(thresholdCount);
+            }
+
+            if (this.getConfig().isAutoUpdateMax() && thresholdCount < typeConfig.getMax()) {
+                listener.getLogger().println("Setting max/stormy value for " 
+                        + typeConfig.getType() + " to " + thresholdCount);
+                typeConfig.setMax(thresholdCount);
+                //fix the min value but don't use fix() because it changes max (and not min)
+                if (typeConfig.getMin() >= typeConfig.getMax()) {
+                	typeConfig.setMin(typeConfig.getMax() - 1);
+                }
+            }
+        }
+    }
+
+	/**
      * Create a project action for a project.
      * @param project the project to create the action for.
      * @return the created violations project action.

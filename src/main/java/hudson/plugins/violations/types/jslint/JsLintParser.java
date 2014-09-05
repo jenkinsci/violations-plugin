dd23adc7b6fd2ee54a16c8999eab7d22ece4a4f1
Bjørnar Grip Fjær <bjornar.fjar@gmail.com>
2014-09-05 20:16:04 +0200

    Fixing JsLint parsing actually showing file content errors

--- src/main/java/hudson/plugins/violations/types/jslint/JsLintParser.java
+++ src/main/java/hudson/plugins/violations/types/jslint/JsLintParser.java
@@ -1,18 +1,20 @@
 package hudson.plugins.violations.types.jslint;
 
+import hudson.plugins.violations.model.FullFileModel;
+import hudson.plugins.violations.model.Severity;
+import hudson.plugins.violations.model.Violation;
+import hudson.plugins.violations.parse.AbstractTypeParser;
+import hudson.plugins.violations.util.AbsoluteFileFinder;
+
+import java.io.File;
 import java.io.IOException;
 
 import org.xmlpull.v1.XmlPullParserException;
 
-import hudson.plugins.violations.model.FullFileModel;
-import hudson.plugins.violations.model.Severity;
-import hudson.plugins.violations.model.Violation;
-
-import hudson.plugins.violations.parse.AbstractTypeParser;
-
 public class JsLintParser extends AbstractTypeParser {
-
     static final String TYPE_NAME = "jslint";
+    
+    private AbsoluteFileFinder absoluteFileFinder = new AbsoluteFileFinder();
     
     /**
      * Parse the JSLint xml file.
@@ -25,6 +27,9 @@ public class JsLintParser extends AbstractTypeParser {
         expectNextTag("jslint");
         getParser().next(); // consume the "jslint" tag
         
+        absoluteFileFinder.addSourcePaths(getSourcePaths());
+        absoluteFileFinder.addSourcePath(getProjectPath().getAbsolutePath());
+        
         // loop thru the child elements, getting the "file" ones
         while (skipToTag("file")) {
             parseFileElement();
@@ -34,9 +39,11 @@ public class JsLintParser extends AbstractTypeParser {
     private void parseFileElement()
         throws IOException, XmlPullParserException {
 
-        String absoluteFileName = fixAbsolutePath(checkNotBlank("name"));
+        String fileName = fixAbsolutePath(checkNotBlank("name"));
+        File file = absoluteFileFinder.getFileForName(fileName);
+
         getParser().next(); // consume "file" tag
-        FullFileModel fileModel = getFileModel(absoluteFileName);
+        FullFileModel fileModel = getFileModel(fileName, file);
 
         // loop thru the child elements, getting the "issue" ones
         while (skipToTag("issue")) {

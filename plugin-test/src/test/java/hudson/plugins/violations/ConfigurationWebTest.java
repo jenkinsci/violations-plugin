1bf25ee72651f9d7b827ce0c095b8bc6665aeb5e
Tomas Bjerre <tomas.bjerre85@gmail.com>
2015-06-16 08:24:31 +0200

    Avoiding crash when setting Maven2 jobs to unstable in 1.609 #61 JENKINS-28880 * Avoiding crash when setting Maven2 build to UNSTABLE. By simply catching the IllegalStateException. This is not the final solution. The build will still not be marked as unstable, instead of crashing (FAIL) it will succeed (SUCCESS). Instead of showing no violation results, it will show them. * Making the example projects compile  * Testing example projects in web tests * Stepping up Jenkins version to 1.580.1 * Adding Java 8 to Travis config  * Using Firefox and Xvfb in Travis CI, HTMLUnit is a bit unpredictable

--- plugin-test/src/test/java/hudson/plugins/violations/ConfigurationWebTest.java
+++ plugin-test/src/test/java/hudson/plugins/violations/ConfigurationWebTest.java
@@ -1,31 +1,32 @@
 package hudson.plugins.violations;
 
-import static com.gargoylesoftware.htmlunit.BrowserVersion.FIREFOX_24;
 import static com.google.common.base.MoreObjects.firstNonNull;
 import static java.lang.System.getProperty;
 import static java.lang.Thread.sleep;
 import static java.util.logging.Level.OFF;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.openqa.selenium.By.id;
 import static org.openqa.selenium.By.xpath;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.apache.commons.logging.LogFactory;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import com.jayway.jsonpath.JsonPath;
 
 public class ConfigurationWebTest {
     private static final String PROP_JENKINS_URL = "jenkins";
-    private static final String PROP_HEADLESS = "headless";
     private static final Logger logger = Logger.getLogger(ConfigurationWebTest.class.getName());
-    private static final String TEST_JOB_NAME = "testJob";
-    private WebDriver webDriver;
+    private static WebDriver webDriver;
 
     static {
         LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log",
@@ -34,123 +35,130 @@ public class ConfigurationWebTest {
         java.util.logging.Logger.getLogger("org.apache.commons.httpclient").setLevel(OFF);
     }
 
-    private String getJenkinsBaseUrl() {
-        return firstNonNull(getProperty(PROP_JENKINS_URL), "http://localhost:8080");
+    private static String getJenkinsBaseUrl() {
+        return firstNonNull(getProperty(PROP_JENKINS_URL), "http://localhost:8080/jenkins");
     }
 
-    private boolean isHeadless() {
-        return !firstNonNull(getProperty(PROP_HEADLESS), "false").equals("false");
-    }
-
-    @Before
-    public void before() throws InterruptedException {
-        if (isHeadless()) {
-            HtmlUnitDriver webDriverHtmlUnit = new HtmlUnitDriver(FIREFOX_24);
-            webDriverHtmlUnit.setJavascriptEnabled(true);
-            this.webDriver = webDriverHtmlUnit;
-        } else {
-            webDriver = new FirefoxDriver();
-        }
+    @BeforeClass
+    public static void before() throws Exception {
+        webDriver = new FirefoxDriver();
         waitForJenkinsToStart();
     }
 
-    @After
-    public void after() {
-        deleteJob();
+    @AfterClass
+    public static void after() throws Exception {
         webDriver.quit();
     }
 
     @Test
-    public void testPluginConfiguration() throws InterruptedException {
-        /**
-         * Create new job and enable plugin on it.
-         */
-        createJob();
-        enablePlugin();
-
-        /**
-         * Enter valid details, start job and validate config output.
-         */
-        webDriver.findElement(xpath("//input[@name='config.limit']")).sendKeys("50");
-        webDriver.findElement(xpath("//input[@name='checkstyle.pattern']")).sendKeys("**/css-checkstyle.xml");
-        removeOnBeforeUnload();
-        saveJob();
-        startJob();
-        waitForJob(1);
-        String consoleText = consoleText(1);
-        assertTrue(consoleText, consoleText.contains("SUCCESS"));
+    public void testThatExampleProjectM2FreedstyleBuildCanBeBuilt() throws Exception {
+        String jobName = "m2-freestyle-build";
+        startJob(jobName);
+        waitForJob(jobName);
+        assertResult(jobName, "unstable");
     }
 
-    private void waitForJob(int index) throws InterruptedException {
-        while (!consoleText(index).contains("Finished: SUCCESS")) {
+    @Test
+    public void testThatExampleProjectM2FreedstyleSiteCanBeBuilt() throws Exception {
+        String jobName = "m2-freestyle-site";
+        startJob(jobName);
+        waitForJob(jobName);
+        assertResult(jobName, "unstable");
+    }
+
+    @Test
+    public void testThatExampleProjectM2M2BuildCanBeBuilt() throws Exception {
+        String jobName = "m2-m2-build";
+        startJob(jobName);
+        waitForJob(jobName);
+        assertResult(jobName, "unstable");
+    }
+
+    @Test
+    public void testThatExampleProjectM2M2SiteCanBeBuilt() throws Exception {
+        String jobName = "m2-m2-site";
+        startJob(jobName);
+        waitForJob(jobName);
+        assertResult(jobName, "unstable");
+    }
+
+    private void assertResult(String jobName, String expectedResult) {
+        webDriver.get(getJenkinsBaseUrl() + "/job/" + jobName + "/lastBuild/api/json?pretty=true");
+        String json = webDriver.getPageSource().replaceAll("<.+?>", "");
+        String result = JsonPath.read(json, "$.['result']");
+        assertEquals("Not unstable!\n" + json, expectedResult.toUpperCase(), result.toUpperCase());
+    }
+
+    private void waitForJob(String jobName) throws InterruptedException {
+        while (!consoleText(jobName).contains("Finished: ")) {
             logger.info("Waiting for jenkins job to finnish");
             sleep(500);
         }
     }
 
-    private String consoleText(int index) {
+    private String consoleText(String jobName) {
         try {
-            webDriver.get(getJenkinsBaseUrl() + "/job/" + TEST_JOB_NAME + "/" + index + "/consoleText");
+            webDriver.get(getJenkinsBaseUrl() + "/job/" + jobName + "/lastBuild/consoleText");
             return webDriver.getPageSource();
         } catch (Exception e) {
             return "";
         }
     }
 
-    private void startJob() {
-        webDriver.get(getJenkinsBaseUrl() + "/job/" + TEST_JOB_NAME + "/build?delay=0sec");
+    private void startJob(String jobName) {
+        webDriver.get(getJenkinsBaseUrl() + "/job/" + jobName + "/build?delay=0sec");
     }
 
-    private void saveJob() {
-        webDriver.findElement(xpath("//button[@id='yui-gen12-button']")).click();
-    }
-
-    private void createJob() {
-        webDriver.get(getJenkinsBaseUrl() + "/view/All/newJob");
-        webDriver.findElement(id("name")).sendKeys(TEST_JOB_NAME);
-        webDriver.findElement(xpath("//input[@value='hudson.model.FreeStyleProject']")).click();
-        webDriver.findElement(id("ok-button")).click();
-    }
-
-    private void enablePlugin() throws InterruptedException {
-        scrollDown();
-        try {
-            webDriver.findElement(xpath("//button[@suffix='publisher']")).click();
-            webDriver.findElement(xpath("//a[text()='Report Violations']")).click();
-        } catch (Exception e) {
-            logger.info("Did not find publisher button");
-        }
-        try {
-            webDriver.findElement(xpath("//input[@name='hudson-plugins-violations-ViolationsPublisher']")).click();
-        } catch (Exception e) {
-            logger.info("Did not find publisher checkbox");
-        }
-        scrollDown();
-    }
-
-    private void deleteJob() {
-        webDriver.get(getJenkinsBaseUrl() + "/job/" + TEST_JOB_NAME + "/delete");
-        webDriver.findElement(xpath("//button[@id='yui-gen1-button']")).click();
-    }
-
-    private void removeOnBeforeUnload() {
-        ((JavascriptExecutor) webDriver).executeScript("window.onbeforeunload=null;", "");
-    }
-
-    private void scrollDown() throws InterruptedException {
-        for (int i = 0; i < 10; i++) {
-            ((JavascriptExecutor) webDriver).executeScript("window.scrollBy(0,1000)", "");
-            sleep(50);
-        }
-    }
-
-    private void waitForJenkinsToStart() throws InterruptedException {
-        webDriver.get(getJenkinsBaseUrl());
+    private static void waitForJenkinsToStart() throws Exception {
         while (webDriver.getPageSource().contains("getting ready")) {
             logger.info("Jenkins not ready...");
             sleep(500);
         }
         logger.info("Jenkins ready!");
         sleep(3000);
+        setMavenHome();
+        sleep(1000);
+    }
+
+    private static void setMavenHome() throws Exception {
+        webDriver.get(getJenkinsBaseUrl() + "/configure");
+        if (!webDriver.findElements(xpath("//input[@value='maven2']")).isEmpty()) {
+            return; // Already configured
+        }
+        waitForAndGet(id("yui-gen8-button")).click();
+        waitForAndGet(xpath("//input[@name='_.name']")).sendKeys("maven2");
+        /**
+         * Looks like a bug in Jenkins here. Sometimes the version is a field
+         * and sometimes a select.
+         */
+        List<WebElement> versionSelect = webDriver.findElements(xpath("//select[@name='_.id']"));
+        if (versionSelect.isEmpty()) {
+            waitForAndGet(xpath("//input[@name='_.id']")).sendKeys("2.2.1");
+        } else {
+            List<WebElement> options = versionSelect.get(0).findElements(By.tagName("option"));
+            for (WebElement option : options) {
+                if (option.getText().startsWith("2.2.1")) {
+                    option.click();
+                    break;
+                }
+            }
+        }
+
+        removeOnBeforeUnload();
+        waitForAndGet(id("yui-gen10-button")).click();
+    }
+
+    private static void removeOnBeforeUnload() {
+        ((JavascriptExecutor) webDriver).executeScript("window.onbeforeunload=null;", "");
+    }
+
+    private static WebElement waitForAndGet(By by) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            if (!webDriver.findElements(by).isEmpty()) {
+                break;
+            }
+            sleep(50);
+        }
+        return webDriver.findElement(by);
     }
 }
--- src/main/java/hudson/plugins/violations/ViolationsCollector.java
+++ src/main/java/hudson/plugins/violations/ViolationsCollector.java
@@ -15,24 +15,21 @@ import java.io.IOException;
 import java.util.logging.Logger;
 
 import org.apache.tools.ant.types.FileSet;
+import org.jenkinsci.remoting.RoleChecker;
 
 /**
- * Collects the violations xml files, analyses them, renders
- * html reports to the working directory, and returns a summary
- * Violations report.
+ * Collects the violations xml files, analyses them, renders html reports to the
+ * working directory, and returns a summary Violations report.
  */
 
 public class ViolationsCollector implements FileCallable<ViolationsReport> {
-   private static final Logger LOG = Logger.getLogger(
-        ViolationsCollector.class.getName());
+    private static final Logger LOG = Logger.getLogger(ViolationsCollector.class.getName());
 
-    private static final String[] NO_STRINGS = new String[]{};
+    private static final String[] NO_STRINGS = new String[] {};
 
     private final boolean mavenProject;
     /** Working directory to copy xml files to. */
     private final FilePath targetDir;
-    private final FilePath htmlDir;
-
     private final ViolationsConfig config;
 
     private FullBuildModel model;
@@ -40,17 +37,19 @@ public class ViolationsCollector implements FileCallable<ViolationsReport> {
 
     /**
      * Constructor.
-     * @param mavenProject true if this a maven project, false otherwise
-     * @param targetDir working directory to copy xml results to
-     * @param htmlDir working directory to html reports to.
-     * @param config  the violations configuration.
+     * 
+     * @param mavenProject
+     *            true if this a maven project, false otherwise
+     * @param targetDir
+     *            working directory to copy xml results to
+     * @param htmlDir
+     *            working directory to html reports to.
+     * @param config
+     *            the violations configuration.
      */
-    public ViolationsCollector(
-        boolean mavenProject, FilePath targetDir, FilePath htmlDir,
-        ViolationsConfig config) {
+    public ViolationsCollector(boolean mavenProject, FilePath targetDir, FilePath htmlDir, ViolationsConfig config) {
         this.mavenProject = mavenProject;
         this.targetDir = targetDir;
-        this.htmlDir = htmlDir;
         this.config = config;
     }
 
@@ -60,13 +59,17 @@ public class ViolationsCollector implements FileCallable<ViolationsReport> {
 
     /**
      * Create a report.
-     * @param workspace the current workspace.
-     * @param channel   the virtual channel.
+     * 
+     * @param workspace
+     *            the current workspace.
+     * @param channel
+     *            the virtual channel.
      * @return the report.
-     * @throws IOException if there is a problem.
+     * @throws IOException
+     *             if there is a problem.
      */
-    public ViolationsReport invoke(File workspace, VirtualChannel channel)
-        throws IOException {
+    @Override
+    public ViolationsReport invoke(File workspace, VirtualChannel channel) throws IOException {
         this.workspace = workspace;
 
         // If the faux project path has been set, use that instead of
@@ -79,16 +82,13 @@ public class ViolationsCollector implements FileCallable<ViolationsReport> {
         String[] sourcePaths = null;
 
         if (mavenProject) {
-            sourcePaths = new String[] {
-                workspace.toString() + "/src/main/java"
-            };
+            sourcePaths = new String[] { workspace.toString() + "/src/main/java" };
         } else {
             // get the source path directories (if any)
-            sourcePaths = findAbsoluteDirs(
-                workspace, config.getSourcePathPattern());
+            sourcePaths = findAbsoluteDirs(workspace, config.getSourcePathPattern());
         }
 
-        for (String sp: sourcePaths) {
+        for (String sp : sourcePaths) {
             LOG.fine("Using extra sourcePath " + sp);
         }
 
@@ -98,7 +98,7 @@ public class ViolationsCollector implements FileCallable<ViolationsReport> {
 
         // Build up the model
         this.model = new FullBuildModel();
-        for (String type: config.getTypeConfigs().keySet()) {
+        for (String type : config.getTypeConfigs().keySet()) {
             TypeConfig c = config.getTypeConfigs().get(type);
             TypeDescriptor typeDescriptor = TypeDescriptor.TYPES.get(type);
             if (typeDescriptor == null) {
@@ -119,7 +119,7 @@ public class ViolationsCollector implements FileCallable<ViolationsReport> {
         // Write out the xml reports
         // ----
         try {
-            new GenerateXML(targetDir , model, config).execute();
+            new GenerateXML(targetDir, model, config).execute();
         } catch (InterruptedException ex) {
             throw new IOException2(ex);
         }
@@ -127,40 +127,35 @@ public class ViolationsCollector implements FileCallable<ViolationsReport> {
         // -----
         // Create the summary report
         // ----
-        for (String type: model.getTypeMap().keySet()) {
-            report.getViolations().put(
-                type, model.getCountNumber(type));
+        for (String type : model.getTypeMap().keySet()) {
+            report.getViolations().put(type, model.getCountNumber(type));
             doSeverities(report, type);
         }
         return report;
     }
 
     /**
-     * Get all the severities of a particular type and
-     * update the severity array of the type summary.
+     * Get all the severities of a particular type and update the severity array
+     * of the type summary.
      */
     private void doSeverities(ViolationsReport report, String type) {
         TypeSummary summary = report.getTypeSummary(type);
-        for (FullFileModel file: model.getFileModelMap().values()) {
+        for (FullFileModel file : model.getFileModelMap().values()) {
             if (file.getTypeMap().get(type) == null) {
                 continue;
             }
-            for (Violation v: file.getTypeMap().get(type)) {
+            for (Violation v : file.getTypeMap().get(type)) {
                 summary.getSeverityArray()[v.getSeverityLevel()]++;
             }
         }
     }
 
-    // TODO: to many if, else, break  and return
-    private void doType(
-        TypeConfig c, TypeDescriptor t, String[] sourcePaths,
-        ViolationsReport report)
-        throws IOException {
+    // TODO: to many if, else, break and return
+    private void doType(TypeConfig c, TypeDescriptor t, String[] sourcePaths, ViolationsReport report)
+            throws IOException {
         String[] fileNames = null;
-        if (mavenProject
-            && t.getMavenTargets() != null
-            && !c.isUsePattern()) {
-            for (String name: t.getMavenTargets()) {
+        if (mavenProject && t.getMavenTargets() != null && !c.isUsePattern()) {
+            for (String name : t.getMavenTargets()) {
                 fileNames = findFiles(workspace, "target/" + name);
                 if (fileNames.length != 0) {
                     break;
@@ -173,39 +168,36 @@ public class ViolationsCollector implements FileCallable<ViolationsReport> {
             fileNames = findFiles(workspace, c.getPattern());
             if (fileNames.length == 0) {
                 if (!mavenProject) {
-                    report.getViolations().put(
-                        c.getType(), -1);
+                    report.getViolations().put(c.getType(), -1);
                     report.getTypeSummary(c.getType()).setErrorMessage(
-                        "No violation report files of type " + c.getType()
-                        + " with pattern " + c.getPattern() + " were found!");
+                            "No violation report files of type " + c.getType() + " with pattern " + c.getPattern()
+                                    + " were found!");
                 }
                 return;
             }
         }
         model.addType(c.getType());
-        for (String fileName: fileNames) {
+        for (String fileName : fileNames) {
             t.createParser().parse(model, workspace, fileName, sourcePaths);
         }
     }
 
-    //  TO DO : PLACE THESE IN A UTILITY CLASS
+    // TO DO : PLACE THESE IN A UTILITY CLASS
     /**
-     * Returns an array with the filenames of the files
-     * that match an Ant pattern using the workspace as the base
-     * directory.
+     * Returns an array with the filenames of the files that match an Ant
+     * pattern using the workspace as the base directory.
      *
-     * @param workspaceRoot root directory of the workspace
+     * @param workspaceRoot
+     *            root directory of the workspace
      *
      * @return the filenames found.
      */
-    private String[] findFiles(
-        final File workspaceRoot, String pattern) {
+    private String[] findFiles(final File workspaceRoot, String pattern) {
         if (StringUtil.isBlank(pattern)) {
             return NO_STRINGS;
         }
         FileSet fileSet = new FileSet();
-        org.apache.tools.ant.Project project
-            = new org.apache.tools.ant.Project();
+        org.apache.tools.ant.Project project = new org.apache.tools.ant.Project();
         fileSet.setProject(project);
         fileSet.setDir(workspaceRoot);
         fileSet.setIncludes(pattern);
@@ -214,22 +206,20 @@ public class ViolationsCollector implements FileCallable<ViolationsReport> {
     }
 
     /**
-     * Returns an array with the relative filenames of directories
-     * that match an Ant pattern using the workspace as the base
-     * directory.
+     * Returns an array with the relative filenames of directories that match an
+     * Ant pattern using the workspace as the base directory.
      *
-     * @param workspaceRoot root directory of the workspace
+     * @param workspaceRoot
+     *            root directory of the workspace
      *
      * @return the filenames found.
      */
-    private String[] findDirs(
-        final File workspaceRoot, String pattern) {
+    private String[] findDirs(final File workspaceRoot, String pattern) {
         if (StringUtil.isBlank(pattern)) {
             return NO_STRINGS;
         }
         FileSet fileSet = new FileSet();
-        org.apache.tools.ant.Project project
-            = new org.apache.tools.ant.Project();
+        org.apache.tools.ant.Project project = new org.apache.tools.ant.Project();
         fileSet.setProject(project);
         fileSet.setDir(workspaceRoot);
         fileSet.setIncludes(pattern);
@@ -238,27 +228,30 @@ public class ViolationsCollector implements FileCallable<ViolationsReport> {
     }
 
     /**
-     * Returns an array with the absolute filenames of directories
-     * that match an Ant pattern using the workspace as the base
-     * directory.
+     * Returns an array with the absolute filenames of directories that match an
+     * Ant pattern using the workspace as the base directory.
      *
-     * @param workspaceRoot root directory of the workspace
+     * @param workspaceRoot
+     *            root directory of the workspace
      *
      * @return the filenames found.
      */
-    private String[] findAbsoluteDirs(
-        final File workspaceRoot, String pattern) {
+    private String[] findAbsoluteDirs(final File workspaceRoot, String pattern) {
         String[] relative = findDirs(workspaceRoot, pattern);
         if (relative.length == 0) {
             return relative;
         }
         String[] absolute = new String[relative.length];
         for (int i = 0; i < relative.length; ++i) {
-            absolute[i] = new File(
-                workspaceRoot, relative[i]).getAbsolutePath();
+            absolute[i] = new File(workspaceRoot, relative[i]).getAbsolutePath();
         }
         return absolute;
     }
 
     private static final long serialVersionUID = 1L;
+
+    // Needed to build with Jenkins 1.609, dont @Override since it will cause
+    // errors when building for older Jenkins
+    public void checkRoles(RoleChecker checker) throws SecurityException {
+    }
 }
